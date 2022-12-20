134ca740fecef1a7638d90a53ee43b175c2dab39
mbaransen <94108242+mbaransen@users.noreply.github.com>
2022-12-20 10:15:38 +0300

    Add files via upload

---
+++ primeNumbers.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void prime()
+//Muzaffer Baran SEN 210208008
+{
+     int i=2,j,p;
+     while(i<=100)
+     {
+          p=1; 
+          for(j=2;j<i;j++)
+          {
+               
+               if( i%j == 0)
+               {
+                    p=0;
+               }
+          }
+          if(p)
+          {
+               printf("\t%d\n",i);
+          }
+     i++;
+     }
+}
+int main()
+{ 
+     printf("Prime Numbers Between 1 to 100\n");
+     printf("---\n");
+     prime();
+     return 0;
+}
